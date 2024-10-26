b49ddba5cc54503d1abca6fe084582fcd66b32fc
Etativel <farhanmaulana4611@gmail.com>
2024-10-26 13:55:01 +0800

    feat: trying some animatin

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const button = document.querySelector(".btn");
-
+const ghostElem = document.querySelector(".hide-me");
+const html = document.querySelector(":root");
+const ball = document.querySelector(".ball");
 button.addEventListener("click", (e) => {
   button.classList.toggle("active");
 });
+
+html.addEventListener("click", () => {
+  showElem();
+});
+
+function showElem() {
+  ghostElem.classList.toggle("showing");
+}
+
+let rotationAngle = 0;
+document.addEventListener(
+  "mousemove",
+  (ev) => {
+    rotationAngle += 10;
+    // console.log(ev);
+    ball.style.transform = `translate(${ev.clientX + 12}px, ${
+      ev.clientY + 12
+    }px) rotate(${rotationAngle}deg)`;
+  },
+  false
+);
